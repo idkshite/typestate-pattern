e617d2ff0ecb2bc68734c0bfbb28f6db21d77292
DATADEER <konto@datadeer.de>
2024-10-03 19:41:28 +0300

    implement email parsing using the typestate-pattern

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use anyhow::{anyhow, Context, Result};
+use regex::Regex;
+
+#[derive(Debug)]
+struct Email(String);
+/*#[derive(Debug)]
+struct Password(String);*/
+
+
+impl Email {
+    fn is_valid_email(email: &String) -> bool{
+    let regex = Regex::new(r"[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+").unwrap();
+    if regex.is_match(email) {
+      return true;
+    }
+
+    false
+    }
+    fn parse(email: String) -> Result<Self>{
+        if Self::is_valid_email(&email){
+            return Ok(Self(email))
+        }
+
+        Err(anyhow!("Invalid Email Address {:?}", email)).context("provided by userId: 123456")
+
+
+    }
+    fn to_str(self) -> String{
+        self.0
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let email = Email::parse("lolman@.com".to_string());
+    match email {
+        Ok(email) => {
+            println!("Hello, {}", email.to_str());
+        }
+        Err(error) => {
+            eprint!("{:?}", error);
+        }
+    }
+
 }
